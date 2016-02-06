f0ce08ca10666fe94a22487d2308b346ed776eaa
hodkovdd <hodkoff2@gmail.com>
2016-02-06 15:23:04 +0300

    connection change to LOCALHOST

---
+++ src/tutorial/Migrations/20160206120547_firstM.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+
+namespace tutorial.Migrations
+{
+    public partial class firstM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_Order_Phone_PhoneId", table: "Order");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Phone_PhoneId",
+                table: "Order",
+                column: "PhoneId",
+                principalTable: "Phone",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_Order_Phone_PhoneId", table: "Order");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Phone_PhoneId",
+                table: "Order",
+                column: "PhoneId",
+                principalTable: "Phone",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
