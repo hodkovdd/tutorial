a23a1b22e3ede1fb241c8364ca2005c4ecee5899
hodkovdd <hodkoff2@gmail.com>
2016-02-06 13:35:23 +0300

    up to 5.2 lesso

--- src/tutorial/Controllers/HomeController.cs
+++ src/tutorial/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Microsoft.AspNet.Mvc;
 using tutorial.Models; // пространство имен моделей
+using System;
 
 namespace MobileStore.Controllers
 {
@@ -11,9 +12,82 @@ namespace MobileStore.Controllers
         {
             db = context;
         }
+        //[ActionName("Welcome")]
         public IActionResult Index()
         {
             return View(db.Phones.ToList());
+        }
+        [HttpGet]
+        public IActionResult Buy(int id)
+        {
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+        //--------------//
+
+
+        //[HttpPost]
+        public string lesson5()
+        {
+            string AltitudeStr = Request.Query.FirstOrDefault(p => p.Key == "altitude").Value;
+            int Altitude = Int32.Parse(AltitudeStr);
+
+            string HeightStr = Request.Query.FirstOrDefault(p => p.Key == "height").Value;
+            int Height = Int32.Parse(HeightStr);
+
+            int Square = Altitude * Height / 2;
+            Geometry geo = new Geometry(Altitude, Height);
+
+            return $"S triangle with a {geo.Altitude} and h {geo.Height} = {geo.GetSquare()}";
+        }
+
+        public string Square()
+        {
+            string altitudeString = Request.Query.FirstOrDefault(p => p.Key == "altitude").Value;
+            int altitude = Int32.Parse(altitudeString);
+
+            string heightString = Request.Query.FirstOrDefault(p => p.Key == "height").Value;
+            int height = Int32.Parse(heightString);
+
+            double square = altitude * height / 2;
+            return $"Площадь треугольника с основанием {altitude} и высотой {height} равна {square}";
+        }
+
+
+
+        public string Sum(Geometry[] geos)
+        { 
+            return $"S squares = {geos.Sum(g=>g.GetSquare())}";
+        }
+
+
+        public IActionResult ForLessons()
+        {
+            return View();
+        }
+    }
+    public class Geometry
+    {
+        public int Altitude { get; set; } // основание
+        public int Height { get; set; } // высота
+        public Geometry(int a, int b)
+        {
+            Altitude = a;
+            Height = b;
+        }
+        
+
+        public double GetSquare() // вычисление площади треугольника
+        {
+            return Altitude * Height / 2;
         }
     }
 }
---
+++ src/tutorial/Migrations/20160205114905_Second.Designer.cs
@@ -0,0 +1,58 @@
+using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using tutorial.Models;
+
+namespace tutorial.Migrations
+{
+    [DbContext(typeof(MobileContext))]
+    [Migration("20160205114905_Second")]
+    partial class Second
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("tutorial.Models.Order", b =>
+                {
+                    b.Property<int>("OrderId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Address");
+
+                    b.Property<string>("ContactPhone");
+
+                    b.Property<int>("PhoneId");
+
+                    b.Property<string>("User");
+
+                    b.HasKey("OrderId");
+                });
+
+            modelBuilder.Entity("tutorial.Models.Phone", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Company");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("Price");
+
+                    b.HasKey("Id");
+                });
+
+            modelBuilder.Entity("tutorial.Models.Order", b =>
+                {
+                    b.HasOne("tutorial.Models.Phone")
+                        .WithMany()
+                        .HasForeignKey("PhoneId");
+                });
+        }
+    }
+}
